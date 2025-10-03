9bdf59edb3b78b378feaae564ab72ef8628c540b
abellpzx <abelucho2006@hotmail.com>
2025-10-03 22:06:46 +0200

    Agregado metodo informe

--- practica1/Main.java
+++ practica1/Main.java
@@ -4,56 +4,56 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         long inicio, fin;
-        double pmin, t1,t2,t3,t4;
+        double pmin, t1, t2, t3, t4;
         String nomfich, extra;
         RedSocial Y = new RedSocial();
         Scanner in = new Scanner(System.in);
 
         System.out.println("        ANALISIS DE Y \n-------------------------------\n");
-        System.out.print("Fichero principal: ");    //Solicitamos fichero principal
+        System.out.print("Fichero principal: "); // Solicitamos fichero principal
         nomfich = in.nextLine();
 
         inicio = System.nanoTime();
-        Y.leeFichero("practica1/"+nomfich);
+        Y.leeFichero("practica1/" + nomfich);
         fin = System.nanoTime();
-        t1 = (fin - inicio)/ 1000000000.0;
+        t1 = (fin - inicio) / 1000000000.0;
 
-        System.out.print("Fichero de nuevas conexiones (pulse enter si no existe): ");  //Solicitamos fichero extra
+        System.out.print("Fichero de nuevas conexiones (pulse enter si no existe): "); // Solicitamos fichero extra
         extra = in.nextLine();
-        if(!extra.equals("")){
-            Y.leeFichero("pracica1/"+extra);
+        if (!extra.equals("")) {
+            Y.leeFichero("pracica1/" + extra);
         }
 
         System.out.print("Porcentaje mayor tamaño grumo: ");
         pmin = in.nextDouble();
 
-        System.out.println("Lectura fichero: "+ t1);
+        System.out.println("Lectura fichero: " + t1);
 
-                // nuevos tiempos
+        // nuevos tiempos
         inicio = System.nanoTime();
         Y.creaUsuarios();
         fin = System.nanoTime();
-        t2 = (fin - inicio)/ 1000000000.0;
+        t2 = (fin - inicio) / 1000000000.0;
         System.out.println("Creacion lista usuarios: " + t2);
 
         inicio = System.nanoTime();
         Y.creaGrumos();
         fin = System.nanoTime();
-        t3 = (fin - inicio)/ 1000000000.0;
+        t3 = (fin - inicio) / 1000000000.0;
         System.out.println("Creacion lista grumos: " + t3);
 
         inicio = System.nanoTime();
         Y.ordenaSelecciona(pmin);
         fin = System.nanoTime();
-        t4 = (fin - inicio)/ 1000000000.0;
-        System.out.println("Ordenacion y seleccion de grumos: "+ t4);
+        t4 = (fin - inicio) / 1000000000.0;
+        System.out.println("Ordenacion y seleccion de grumos: " + t4);
 
         System.out.println(Y.numUsuarios() + " usuarios, " + Y.numConexiones() + " conexiones");
         System.out.println("Existen " + Y.numGrumos() + " grumos.");
 
+        Y.informe();
 
         in.close();
     }
 
-    
 }
--- practica1/RedSocial.java
+++ practica1/RedSocial.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.io.IOException;
 import java.util.*;
 
 public class RedSocial implements IRedSocial {
@@ -9,8 +8,9 @@ public class RedSocial implements IRedSocial {
     ArrayList<ArrayList<Integer>> grus = new ArrayList<>();
     ArrayList<Integer> asig = new ArrayList<>();
 
-    ArrayList<Conexion> nuevas = new ArrayList<>(); // array para las nuevas conexiones 
-    ArrayList<ArrayList<Integer>> grusSeleccionados = new ArrayList<>(); // array para obtener los grumos utilizados para el porcentaje
+    ArrayList<Conexion> nuevas = new ArrayList<>(); // array para las nuevas conexiones
+    ArrayList<ArrayList<Integer>> grusSeleccionados = new ArrayList<>(); // array para obtener los grumos utilizados
+                                                                         // para el porcentaje
 
     public int numUsuarios() {
         return usr.size();
@@ -29,9 +29,9 @@ public class RedSocial implements IRedSocial {
         Scanner in = new Scanner(new File(nomfich));
         int u1, u2;
 
-        while(in.hasNextInt()){
-            u1=in.nextInt();
-            u2=in.nextInt();
+        while (in.hasNextInt()) {
+            u1 = in.nextInt();
+            u2 = in.nextInt();
 
             red.add(new Conexion(u1, u2));
         }
@@ -46,20 +46,20 @@ public class RedSocial implements IRedSocial {
 
     public void creaUsuarios() {
 
-        int u1,u2;
-        for (Conexion c : red){
+        int u1, u2;
+        for (Conexion c : red) {
 
             u1 = c.getc1();
             u2 = c.getc2();
 
-            if(!usr.contains(u1)){
+            if (!usr.contains(u1)) {
                 usr.add(u1);
             }
 
-            if(!usr.contains(u2)){
+            if (!usr.contains(u2)) {
                 usr.add(u2);
             }
-            
+
         }
 
     }
@@ -68,8 +68,8 @@ public class RedSocial implements IRedSocial {
 
         ArrayList<Integer> grumo;
 
-        for(Integer u : usr){
-            if(!asig.contains(u)){
+        for (Integer u : usr) {
+            if (!asig.contains(u)) {
                 grumo = new ArrayList<>();
                 grumo.add(u);
                 uber_amigos(u, red, grumo);
@@ -82,47 +82,47 @@ public class RedSocial implements IRedSocial {
 
     public void ordenaSelecciona(double pmin) {
 
-        int acumulado=0;
+        int acumulado = 0;
         double porcentaje;
         boolean superado = false;
-        int u1,u2;
+        int u1, u2;
 
-        grus.sort( (l1,l2) -> l2.size()-l1.size() );
+        grus.sort((l1, l2) -> l2.size() - l1.size()); // ordena los grumos de mayor a menor tamaño
 
-        for(ArrayList<Integer> grumo : grus){
-            if(!superado){
+        for (ArrayList<Integer> grumo : grus) {
+            if (!superado) {
 
-                acumulado+=grumo.size();
-                porcentaje = (double) acumulado/numUsuarios() *100.0;
+                acumulado += grumo.size();
+                porcentaje = (double) acumulado / numUsuarios() * 100.0;
                 grusSeleccionados.add(grumo);
 
-                if(porcentaje >=pmin){
+                if (porcentaje >= pmin) {
                     superado = true;
                 }
-            }      
+            }
         }
 
-        if(grusSeleccionados.size() >1){
-            for(int i=0;i<grusSeleccionados.size()-1;i++){
+        if (grusSeleccionados.size() > 1) {
+            for (int i = 0; i < grusSeleccionados.size() - 1; i++) {
 
                 ArrayList<Integer> grumo1 = grusSeleccionados.get(i);
-                ArrayList<Integer> grumo2 = grusSeleccionados.get(i+1);
+                ArrayList<Integer> grumo2 = grusSeleccionados.get(i + 1);
 
                 u1 = grumo1.get(1);
                 u2 = grumo2.get(0);
-                
+
                 nuevas.add(new Conexion(u1, u2));
             }
         }
-       
+
     }
 
     public void salvaNuevasRel(String nomfich) throws IOException {
 
         PrintWriter extra;
-        if(grusSeleccionados.size()>1){
+        if (grusSeleccionados.size() > 1) {
             extra = new PrintWriter(nomfich);
-            for(Conexion c : nuevas){
+            for (Conexion c : nuevas) {
                 extra.println(c);
             }
 
@@ -131,24 +131,50 @@ public class RedSocial implements IRedSocial {
     }
 
     public void informe() {
+        int tam;
+        double porcentaje;
+        ArrayList<Integer> grumo = new ArrayList<>();
 
+        if (grusSeleccionados.size() == 1) { // CASO: No se necesitan relaciones nuevas
+            grumo = grusSeleccionados.get(0);
+            tam = grumo.size();
+            porcentaje = (tam * 100.0) / usr.size();
+            porcentaje = Math.round(porcentaje * 100.0) / 100.0; // redondea a dos decimales
+            System.out.println("El mayor grumo contiene " + tam + " usuarios (" + porcentaje + "%)");
+            System.out.println("No son necesarias nuevas relaciones de amistad");
+
+        } else { // CASO: Se necesitan relaciones nuevas
+            System.out.println("Se deben unir los " + grusSeleccionados.size() + " mayores");
+            for (int i = 0; i < grusSeleccionados.size(); i++) {
+                grumo = grusSeleccionados.get(i);
+                tam = grumo.size();
+                porcentaje = (tam * 100.0) / usr.size();
+                porcentaje = Math.round(porcentaje * 100.0) / 100.0; // redondea a dos decimales
+                System.out.println("#" + (i + 1) + ": " + tam + " usuarios (" + porcentaje + "%)");
+            }
+
+            System.out.println("Nuevas relaciones de amistad (salvadas en extra.txt)");
+            for (Conexion c : nuevas) {
+                System.out.println(c);
+            }
+        }
     }
 
-    private void uber_amigos(int usuario, ArrayList<Conexion> red, ArrayList<Integer> grumo){
-        
-        int amigo,u1,u2;
-        for (Conexion c : red){
+    private void uber_amigos(int usuario, ArrayList<Conexion> red, ArrayList<Integer> grumo) {
+
+        int amigo, u1, u2;
+        for (Conexion c : red) {
             amigo = -1;
             u1 = c.getc1();
             u2 = c.getc2();
 
-            if(usuario == u1 ){
+            if (usuario == u1) {
                 amigo = u2;
-            } else if(usuario == u2){
+            } else if (usuario == u2) {
                 amigo = u1;
             }
 
-            if(amigo != -1 && !grumo.contains(amigo)){
+            if (amigo != -1 && !grumo.contains(amigo)) {
                 grumo.add(amigo);
                 uber_amigos(amigo, red, grumo);
             }
