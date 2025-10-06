f1ce7ccb6f2eb614ea2313174947c1df25023bb8
abellpzx <abelucho2006@hotmail.com>
2025-10-06 19:00:14 +0200

    Main Tiempo v2

--- practica1/MainTiempo.java
+++ practica1/MainTiempo.java
@@ -1,50 +1,48 @@
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
 
 public class MainTiempo {
 
-    final int Ninicio = 0, Nrep = 0, Ninc = 0, Nfin = 0;
+    static final int Ninicio = 0, Nrep = 0, Ninc = 0, Nfin = 0;
+    static final Random RND = new Random(123456789);
     public static void main(String[] args) throws IOException{
-        
-        long inicio, fin;
-        double pmin, t1, t2, t3, t4;
-        String nomfich, extra;
-        RedSocial Y = new RedSocial();
-        Scanner in = new Scanner(System.in);
+        for(int i=0;i<Nrep;i++){
 
-        nomfich = in.nextLine();
+            long inicio, fin;
+            double pmin, t1, t2, t3, t4;
+            String nomfich, extra;
+            RedSocial Y = new RedSocial();
 
-        inicio = System.nanoTime();
-        Y.leeFichero("practica1/" + nomfich);
-        fin = System.nanoTime();
-        t1 = (fin - inicio) / 1000000000.0;
+            inicio = System.nanoTime();
+            List<GeneradorCasosPrueba.Conexion> generador = GeneradorCasosPrueba.generaCaso(Ninicio, RND);
+            //Y.setRed(generador);
+            fin = System.nanoTime();
+            t1 = (fin - inicio) / 1000000000.0;
 
-        extra = in.nextLine();
-        if (!extra.equals("")) {
-            Y.leeFichero("practica1/" + extra);
+
+
+            pmin = 90;
+
+            // nuevos tiempos
+            inicio = System.nanoTime();
+            Y.creaUsuarios();
+            fin = System.nanoTime();
+            t2 = (fin - inicio) / 1000000000.0;
+
+            inicio = System.nanoTime();
+            Y.creaGrumos();
+            fin = System.nanoTime();
+            t3 = (fin - inicio) / 1000000000.0;
+
+            inicio = System.nanoTime();
+            Y.ordenaSelecciona(pmin);
+            fin = System.nanoTime();
+            t4 = (fin - inicio) / 1000000000.0;
+
+
         }
-
-        pmin = in.nextDouble();
-
-        // nuevos tiempos
-        inicio = System.nanoTime();
-        Y.creaUsuarios();
-        fin = System.nanoTime();
-        t2 = (fin - inicio) / 1000000000.0;
-
-        inicio = System.nanoTime();
-        Y.creaGrumos();
-        fin = System.nanoTime();
-        t3 = (fin - inicio) / 1000000000.0;
-
-        inicio = System.nanoTime();
-        Y.ordenaSelecciona(pmin);
-        fin = System.nanoTime();
-        t4 = (fin - inicio) / 1000000000.0;
-
-
-        in.close();
     }
-
 }
 
