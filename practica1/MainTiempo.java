d1a340777c0e97ad4a524f5051f189fd7df172a4
masanjo <mario.san-jose.prado23@estudiantes.uva.es>
2025-10-30 10:32:47 +0100

    fin_v4

--- practica1/MainTiempo.java
+++ practica1/MainTiempo.java
@@ -18,14 +18,14 @@ import java.util.List;
  */
 public class MainTiempo {
 
-    static int Nrep = 10, Ninc = 5000, Nfin = 200000;
+    static int Nrep = 10, Ninc = 100, Nfin = 200000;
     static final Random RND = new Random();
 
     public static void main(String[] args) throws IOException {
         double pmin, t2, t3, t4, tTotal;
         double promedio2, promedio3, promedio4;
         int promedioConexiones, promedioGrumos, conexiones, grumos;
-        int Ninicio = 5000;
+        int Ninicio = 100;
         List<Conexion> generador;
 
         while (Ninicio <= Nfin) {
@@ -85,9 +85,25 @@ public class MainTiempo {
 
             tTotal = promedio2 + promedio3 + promedio4;
             System.out.println("Tiempo total: " + tTotal);
+
+            if(Ninicio == 500){
+                Ninc = 500;
+            }
+            if(Ninicio == 5000){
+                Ninc = 5000;
+            }
+
+            if(Ninicio == 75000){
+                Nrep=6;
+            }
+
             if (Ninicio == 100000) {
                 Nrep = 3; // cambio de nº repes al 100k
-                Ninc = 15000; // cambio de incrementos al 100k
+                Ninc = 10000; // cambio de incrementos al 100k
+            }
+            
+            if (Ninicio == 150000){
+                Ninc = 20000;
             }
             Ninicio += Ninc;
         }
